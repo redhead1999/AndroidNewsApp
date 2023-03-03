2bc2c4e27a0196e626bc7cce0956efa41c109ac0
Кирилин Юрий Александрович <kirilin.ya@avgd.pro>
2023-03-03 10:32:11 +0400

    news_feature implementation MAPPING

---
+++ news_feature_impl/src/main/java/com/aold/news_feature_impl/data/mappers/NewsDetailsToDomainMapper.kt
@@ -0,0 +1,23 @@
+package com.aold.news_feature_impl.data.mappers
+
+import com.aold.core.core.common.Mapper
+import com.aold.core_db.details.NewsDetailsModel
+import com.aold.news_feature_impl.domain.entities.NewsEntity
+import javax.inject.Inject
+
+/**
+ * @author {Kirilin Yury} on 03.03.2023.
+ */
+internal interface NewsDetailsToDomainMapper : Mapper<NewsEntity, NewsDetailsModel> {
+
+    class Base @Inject constructor() : NewsDetailsToDomainMapper {
+
+        override fun map(input: NewsEntity) = NewsDetailsModel(
+            content = input.content,
+            source = input.source,
+            sourceUrl = input.sourceUrl,
+            imageUrl = input.image,
+            publishedAt = input.publishedAt
+        )
+    }
+}
---
+++ news_feature_impl/src/main/java/com/aold/news_feature_impl/data/mappers/NewsResponseToModelMapper.kt
@@ -0,0 +1,29 @@
+package com.aold.news_feature_impl.data.mappers
+
+import com.aold.core.core.common.Mapper
+import com.aold.news_feature_impl.data.models.NewsModel
+import com.aold.news_feature_impl.data.models.NewsResponse
+import javax.inject.Inject
+
+/**
+ * @author {Kirilin Yury} on 03.03.2023.
+ */
+
+internal interface NewsResponseToModelMapper : Mapper<NewsResponse, List<NewsModel>> {
+
+    class Base @Inject constructor() : NewsResponseToModelMapper {
+
+        override fun map(input: NewsResponse) = input.articles.map {
+            NewsModel(
+                title = it.title,
+                description = it.description,
+                content = it.content,
+                sourceUrl = it.url,
+                image = it.image,
+                publishedAt = it.publishedAt,
+                sourceName = it.source?.name,
+                baseSourceUrl = it.source?.url
+            )
+        }
+    }
+}
